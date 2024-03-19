0dc929e2c7749366c0ccba5f92f6718d1609f722
markiiiikram <152339178+markiiiikram@users.noreply.github.com>
2024-03-19 15:36:48 +0000

    Added google maps to the offices, added the breadcrumbs to the top of the page. Currently not implemented by PHP but will attempt to implement this when I have time.

--- inc/news.php
+++ inc/news.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
 }
 
 // Step 2: Execute a query
-$sql = "SELECT n.title, n.minute_read, n.content, n.posted_by_image, n.posted_by, n.date, n.image, n.image_alt, c.color
+$sql = "SELECT n.title, n.minute_read, n.content, n.posted_by_image, n.posted_by_image_alt, n.posted_by, n.date, n.image, n.image_alt, c.color
         FROM news AS n
         JOIN color AS c ON n.color_ID = c.color_ID
         LIMIT 3;";
@@ -29,21 +29,15 @@ if ($result && $result->num_rows > 0) {
     for ($i = 1; $i <= 3; $i++) {
         // Fetch row data
         $row = $result->fetch_assoc();
-        
-
-
-
-
-        // Output news container for each row
         echo '<div class="article-container float-container container-col-' . $i . '">';
         echo '<a href="#">';
-        echo '<img class="employees" alt="' . $row['posted_by'] . '" src="' . $row['posted_by_image'] . '">';
+        echo '<img class="employees" alt="' . $row['image_alt'] . '" src="' . $row['image'] . '">';
         echo '<div class="article-details">';
         echo '<h3 class="article-h3">' . $row['title'] . ' <span class="read-time">- ' . $row['minute_read'] . ' minute read</span></h3>';
         echo '<p>' . $row['content'] . '</p>';
-        echo '<span class="button-blue btn" id="button-g">Read More</span>';
+        echo '<span class="button-' . $row['color'] . ' btn" id="button-g">Read More</span>';
         echo '<div class="posted">';
-        echo '<img src="' . $row['image'] . '" alt="' . $row['image_alt'] . '" class="min-logo">';
+        echo '<img src="' . $row['posted_by_image'] . '" alt="' . $row['posted_by_image_alt'] . '" class="min-logo">';
         echo '<div class="flex"><strong class="text-primary">Posted by ' . $row['posted_by'] . '</strong><br>' . $row['date'] . '</div>';
         echo '</div></div></a>';
         echo '<div class="image-container"><a href="#">NEWS</a></div></div>';
--- support.php
+++ support.php
@@ -11,7 +11,16 @@
 <?php include_once './inc/cookie.php' ?>
 <?php include_once './inc/header.php' ?>
 <?php include_once './inc/nav-bar.php' ?>
-    <div class="breadcrumbs container"></div>
+    
+<div class="breadcrumbs">
+    <div class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li> / </li>
+                <li>Our Offices</li>
+            </ul>
+        </div>
+</div>
     <div class="h1-container">
         <div class="container">
          <h1 class="h1-office">Our Offices</h1>
@@ -20,35 +29,40 @@
     <div class="middle container">
     <div class="top-section"></div>
     <div class="offices">
-        <div class="london office">
-            <a href="#" class="office-img">
-                <img src="images/banner/london.jpg" alt="london">
-            </a>
-            <div class="content">
-                <div class="h2">
-                    <a href="#">London Office</a>
-                </div>
-                <p class="p">
-                    Unit G6,
-                    <br>
-                    Pixel Business Centre,
-                    <br>
-                    110 Brooker Road,
-                    Waltham Abbey,
-                    <br>
-                    London,
-                    <br>
-                    EN9 1JH
-                </p>
-                <div class="tele">
-                    <a href="#" >02045 397354</a>
-                </div>
-                <div class="view-more con-purp">
-                    <a href="" class="view-more-button ">VIEW MORE</a>
+        <div class="office-container ">
+            <div class="london office">
+                <a href="#" class="office-img">
+                    <img src="images/banner/london.jpg" alt="london">
+                </a>
+                <div class="content">
+                    <div class="h2">
+                        <a href="#">London Office</a>
+                    </div>
+                    <p class="p">
+                        Unit G6,
+                        <br>
+                        Pixel Business Centre,
+                        <br>
+                        110 Brooker Road,
+                        Waltham Abbey,
+                        <br>
+                        London,
+                        <br>
+                        EN9 1JH
+                    </p>
+                    <div class="tele">
+                        <a href="#" >02045 397354</a>
+                    </div>
+                    <div class="view-more con-purp">
+                        <a href="" class="view-more-button ">VIEW MORE</a>
+                    </div>
                 </div>
             </div>
-
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1236.9012519929447!2d-0.002868!3d51.681758!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fff09271cbd%3A0xbfed51d9cf061321!2sPixel%20Business%20Centre%2C%20Brooker%20Rd%2C%20Waltham%20Abbey%20EN9%201JH!5e0!3m2!1sen!2suk!4v1710859574845!5m2!1sen!2suk" width="100%" height="250" style="border-bottom-left-radius: 20px;
+            border-bottom-right-radius: 20px;
+            border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" id="google-maps"></iframe>
         </div>
+        <div class="office-container ">
         <div class="cambridge office">
             <a href="#" class="office-img">
                 <img src="images/banner/cambridge.jpg" alt="london">
@@ -75,7 +89,10 @@
                         <a href="" class="view-more-button">VIEW MORE</a>
                     </div>
             </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.4449748640736!2d0.1513851765274437!3d52.23530085726904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d871ace1fea115%3A0x3b83f818538541b8!2sSt%20John's%20Innovation%20Centre!5e0!3m2!1sen!2suk!4v1709209752309!5m2!1sen!2suk" width="100%" height="250"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
+        <div class="office-container ">
         <div class="wymondham office">
             <a href="#" class="office-img">
                 <img src="images/banner/wymondham.jpg" alt="london">
@@ -103,11 +120,14 @@
                     </div>
             </div>
         </div>
-        <div class="yarmouth office">
-        <a href="#" class="office-img">
-                <img src="images/banner/yarmouth-2.jpg" alt="london">
-            </a>
-            <div class="content">
+        <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2424.646653310804!2d1.131838890416635!3d52.57600096009048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9ddacf5488fd7%3A0x72ab854c586cb9ed!2s15%20Penfold%20Dr%2C%20Wymondham%20NR18%200WZ!5e0!3m2!1sen!2suk!4v1709209806892!5m2!1sen!2suk" width="100%" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="office-container ">
+            <div class="yarmouth office">
+                <a href="#" class="office-img">
+                        <img src="images/banner/yarmouth-2.jpg" alt="london">
+                </a>
+                <div class="content">
                     <div class="h2">
                         <a href="#">Great Yarmouth Office</a>
                     </div>
@@ -123,12 +143,14 @@
                         NR18 0WZ
                     </p>
                     <div class="tele">
-                         <a href="#" >01603 70 40 20</a>
+                            <a href="#" >01603 70 40 20</a>
                     </div>
                     <div class="view-more con-purp">
                         <a href="" class="view-more-button ">VIEW MORE</a>
                     </div>
+                </div>
             </div>
+            <iframe class="office-map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2425.7627527589448!2d1.710404476545653!3d52.555816233577374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47da04f3998cbd9b%3A0x95f38a894b85cdc6!2sBeacon%20Innovation%20Centre%2C%20Camelot%20Rd%2C%20Gorleston-on-Sea%2C%20Great%20Yarmouth%20NR31%207RA!5e0!3m2!1sen!2suk!4v1709209891765!5m2!1sen!2suk" width="600" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
     <div class="bottom-section">
